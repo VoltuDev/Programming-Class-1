52d47e8415f4258b961239fdfe5fd685cd516604
Voltu <tammistehugo@gmail.com>
2024-02-26 22:42:01 +0200

    Add week 1 files

---
+++ index.js
@@ -0,0 +1,24 @@
+const profileImage = document.querySelector("#profileImage");
+const firstNameSpan = document.querySelector("#firstName");
+const ageSpan = document.querySelector("#age");
+const statusSpan = document.querySelector("#status");
+const btnUpdateProfile = document.querySelector("#updateBtn");
+
+const isMonkey = confirm("Are you a monkey?");
+
+if(isMonkey === true){
+    console.log("Welcome to Monkeylandia!");
+} 
+else {
+    profileImage.src = "images/image2.jpg"; 
+    firstNameSpan.innerHTML = "Cheetah";
+    ageSpan.innerHTML = 80;
+    statusSpan.innerHTML = "Monkeyman";
+}
+
+btnUpdateProfile.addEventListener("click", () => {
+    profileImage.src = "images/image4.jpg"; 
+    firstNameSpan.innerHTML = "King Kong";
+    ageSpan.innerHTML = 17;
+    statusSpan.innerHTML = "Ape";
+});
